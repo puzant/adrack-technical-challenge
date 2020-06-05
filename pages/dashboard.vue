eae64b032cbd83b2910e411290fc47d9e5427f7a
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-05 19:50:11 +0300

    get the data via get request and pass it to the component

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container">
     <h1>Dashboard Page</h1>
-    <ChartLine />
+
+    <div class="loader-container">
+      <img v-if="!loaded" class="chart-loader mt-3" src="../static/loader-dotted.gif" alt="">
+    </div>
+
+    <ChartLine v-if="loaded" :chartData="values"/>
   </div>  
 </template>
 
@@ -11,6 +16,13 @@ export default {
   middleware: 'session',
   components: {
     ChartLine
+  },
+  data() {
+    return {
+      values: [],
+      customValues: [],
+      loaded: false
+    }
   },
   head() {
     return {
@@ -23,10 +35,36 @@ export default {
         }
       ]
     }
+  },
+  mounted() {
+    this.requestData()
+  },
+  methods: {
+    requestData() {
+      this.loaded = false
+      //  added a delay of 2 seconds to simulate a real server delay
+      fetch(" http://www.mocky.io/v2/5eda474f330000fefc79eab4?mocky-delay=2000ms")
+        .then(response => response.json())
+        .then(json => {
+        this.values = json.data.value
+        this.loaded = true
+      })
+    },
   }
 }
 </script>
 
 <style>
 
+</style>
+
+<style>
+.loader-container {
+  display: flex;
+  justify-content: center;
+}
+
+.chart-loader {
+  width: 150px;
+}
 </style>
