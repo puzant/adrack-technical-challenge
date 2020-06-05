14783b5830fe1fe2686061f2bc52154f98cecd81
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-05 19:49:42 +0300

    add a new line chart component

--- components/chart-line.vue
+++ components/chart-line.vue
@@ -1,79 +1,40 @@
-<template>
-  <div class="card">
-    <!-- <div class="card-body">
-      <h2 class="card-title">Line</h2>
-
-      <div class="btn-group btn-group-toggle">
-        <label
-          v-for="(item, index) in btn"
-          :key="index"
-          :class="{ active: item.value == radio }"
-          class="btn btn-success"
-        >
-          <input
-            v-model="radio"
-            :name="dataLabel"
-            :value="item.value"
-            type="radio"
-          />
-          {{ item.label }}
-        </label>
-      </div>
-    </div> -->
-
-    <div class="loader-container">
-      <img v-if="!loaded" class="chart-loader mt-3" src="../static/loader-dotted.gif" alt="">
-    </div>
-
-    <div class="card-img-bottom" v-if="loaded">
-      <chartjs-line
-        :backgroundcolor="bgColor"
-        :beginzero="beginZero"
-        :bind="true"
-        :bordercolor="borderColor"
-        :data="chartData.value"
-        :datalabel="dataLabel"
-        :labels="chartData.week"
-      />
-    </div>
-  </div>
-</template>
-
 <script>
-export default {
-  data() {
-    return {
-      errorDetected: false,
-      loaded: false,
-      chartData: {},
-      bgColor: "#5b88e3",
-      beginZero: true,
-      borderColor: "#5b88e3",
-      dataLabel: "Test",
-    };
-  },
+  //Importing Line class from the vue-chartjs wrapper
+  import { Line, mixins } from 'vue-chartjs'
+  const { reactiveProp } = mixins
 
-  mounted() {
-    this.loaded = false
-    //  added a delay of 4 seconds to simulate a real server delay
-    fetch("http://www.mocky.io/v2/5ed90ffd31000053b2c4eb09?mocky-delay=3000ms")
-      .then(response => response.json())
-      .then(json => {
-        this.chartData = json
-        this.loaded = true
-      })
-  },
 
-};
+  //Exporting this so it can be used in other components
+  export default {
+    extends: Line,
+    mixins: [reactiveProp],
+    props: ['chartData'],
+    data () {
+      return {
+        datacollection: {
+          //Data to be represented on x-axis
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+            {
+              label: "Data 1",
+              backgroundColor: "transparent",
+              borderColor: "rgba(1, 116, 188, 0.50)",
+              pointBackgroundColor: "rgba(171, 71, 188, 1)",
+              //Data to be represented on y-axis
+              data: this.chartData
+            }
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false
+        }
+        //Chart.js options that controls the appearance of the chart
+      }
+    },
+    mounted () {
+      //renderChart function renders the chart with the datacollection and options object.
+      this.renderChart(this.datacollection, this.options)
+    }
+  }
 </script>
-
-<style>
-.loader-container {
-  display: flex;
-  justify-content: center;
-}
-
-.chart-loader {
-  width: 150px;
-}
-</style>
