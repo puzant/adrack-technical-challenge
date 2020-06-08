26a8a1ed0b54cc01d7dba2365e86e5dea3281571
Puzant <PBAKJEJIAN@hotmail.com>
2020-06-08 13:36:54 +0300

    remove the dataset from inside the chart-line

--- components/chart-line.vue
+++ components/chart-line.vue
@@ -1,30 +1,28 @@
 <script>
   //Importing Line class from the vue-chartjs wrapper
   import { Line, mixins } from 'vue-chartjs'
-  const { reactiveProp } = mixins
-
 
   //Exporting this so it can be used in other components
   export default {
     extends: Line,
-    mixins: [reactiveProp],
-    props: ['chartData'],
+    mixins: [mixins.reactiveProp],
+    // props:['chartData'],
     data () {
       return {
-        datacollection: {
-          //Data to be represented on x-axis
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          datasets: [
-            {
-              label: "Data 1",
-              backgroundColor: "transparent",
-              borderColor: "rgba(1, 116, 188, 0.50)",
-              pointBackgroundColor: "rgba(171, 71, 188, 1)",
-              //Data to be represented on y-axis
-              data: this.chartData
-            }
-          ],
-        },
+        // datacollection: {
+        //   //Data to be represented on x-axis
+        //   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        //   datasets: [
+        //     {
+        //       label: "Data 1",
+        //       backgroundColor: "transparent",
+        //       borderColor: "rgba(1, 116, 188, 0.50)",
+        //       pointBackgroundColor: "rgba(171, 71, 188, 1)",
+        //       //Data to be represented on y-axis
+        //       data: this.chartData
+        //     }
+        //   ],
+        // },
         options: {
           responsive: true,
           maintainAspectRatio: false
@@ -32,14 +30,9 @@
         //Chart.js options that controls the appearance of the chart
       }
     },
-    watch: {
-      chartData() {
-        this.renderChart(this.datacollection, this.options);
-      }
-    },
     mounted () {
       //renderChart function renders the chart with the datacollection and options object.
-      this.renderChart(this.datacollection, this.options)
+      this.renderChart(this.chartData, this.options)
     }
   }
 </script>
